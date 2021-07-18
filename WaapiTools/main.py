74a44ac3b49d8f5207c64f97173c5a5304d8545e
johnlsoer <john-masterchief@hotmail.com>
2021-07-19 00:08:31 +0800

    使用Waapi重新设计功能

--- WaapiTools/main.py
+++
@@ -1,3 +0,0 @@
-from file_importer import  *
-
-import_and_create_event("D:\\Sound Design\\ElderScrollsUI\\Elder_Scrolls_UI_Pack_John_Yang")
--- WwiseTools/Program.cs
+++ WwiseTools/Program.cs
@@ -3,11 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WwiseTools.Properties;
 using WwiseTools.Utils;
-using WwiseTools.Audio;
-using WwiseTools.Basics;
-using WwiseTools.Reference;
 
 namespace WwiseTools
 {
@@ -15,23 +11,8 @@ namespace WwiseTools
     {
         static void Main(string[] args)
         {
-            WwiseTools.Utils.WwiseUtility.Init(@"../../../TestProject", @"C:\", false);//初始化Wwise工程路径
-            WwiseTools.Utils.WwiseParser parser = new WwiseTools.Utils.WwiseParser();
-            parser.InitWorkUnit("Test", "Events", "Events/Test.wwu");
-
-            WwiseEvent ev = new WwiseEvent("PlayShit", parser);
-            WwiseSound sound = new WwiseSound("Shit", "SFX", "test.wav", parser);
-            sound.SetInclusion(false);
-            WwiseObjectRef reference = new WwiseObjectRef("Shit", sound.ID, parser);
-            ev.AddChild
-                (
-                    new WwiseAction(WwiseAction.ActionType.Play, reference, parser)
-                );
-
-            parser.AddChildToWorkUnit(ev);
-
-            parser.ToFile("test.xml");
-            parser.CommitChange();
+            WwiseUtility.Init(@"../../../TestProject", @"C:\", false).Wait();
+            WwiseUtility.ImportFile("D:\\UI_Cancel.wav", "Chinese(CN)").Wait();
 
             Console.ReadLine();
         }
