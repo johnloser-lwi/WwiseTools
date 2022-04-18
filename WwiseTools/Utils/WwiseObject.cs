e392b9d72c30e1b49daab8ef5aaf6c111a8c1710
yangweiqin <yangweiqin@xd.com>
2022-04-18 11:39:05 +0800

    修复部分bug

--- WwiseTools/Utils/WwiseObject.cs
+++ WwiseTools/Utils/WwiseObject.cs
@@ -10,7 +10,7 @@ namespace WwiseTools.Objects
 {
     public class WwiseObject
     {
-        public enum ObjectType { AcousticTexture, Action, ActionException, ActorMixer, Attenuation, AudioDevice, AuxBux, BlendContainer, BlendTrack, Bus, ControlSurfaceBinding, ControlSurfaceBindingGroup, ControlSurfaceSession, Conversion, Curve, CustomState, DialogueEvent, Deffect, Event, ExternalSource, ExternalSourceFile, Folder, GameParameter, Language, Metadata, MidiParameter, MixingSession, Modifier, ModulatorEnvelope, ModulatorLfo, ModulatorTime, MultiSwitchEntry, MusicClip, MusicClipMidi, MusicCue, MusicEventCue, MusicFade, MusicPlaylistContainer, MusicPlaylistItem, MusicSegment, MusicStinger, MusicSwitchContainer, MusicTrack, MusicTrackSequence, MusicTransition, ObjectSettingAssoc, Panner, ParamControl, Path, Platform, PluginDataSource, Position, Project, Query, RandomSequenceContainer, SerchCriteria, Sound, SoundBank, SoundcasterSession, State, StateGroup, Switch, SwitchContainer, SwitchGroup, Trigger, UserProjectSettings, WorkUnit, SegmentRef }
+        public enum ObjectType { AcousticTexture, Action, ActionException, ActorMixer, Attenuation, AudioDevice, AuxBus, BlendContainer, BlendTrack, Bus, ControlSurfaceBinding, ControlSurfaceBindingGroup, ControlSurfaceSession, Conversion, Curve, CustomState, DialogueEvent, Deffect, Event, ExternalSource, ExternalSourceFile, Folder, GameParameter, Language, Metadata, MidiParameter, MixingSession, Modifier, ModulatorEnvelope, ModulatorLfo, ModulatorTime, MultiSwitchEntry, MusicClip, MusicClipMidi, MusicCue, MusicEventCue, MusicFade, MusicPlaylistContainer, MusicPlaylistItem, MusicSegment, MusicStinger, MusicSwitchContainer, MusicTrack, MusicTrackSequence, MusicTransition, ObjectSettingAssoc, Panner, ParamControl, Path, Platform, PluginDataSource, Position, Project, Query, RandomSequenceContainer, SerchCriteria, Sound, SoundBank, SoundcasterSession, State, StateGroup, Switch, SwitchContainer, SwitchGroup, Trigger, UserProjectSettings, WorkUnit, SegmentRef }
 
         public string Name { get; set; }
         public string ID { get; set; }
@@ -26,6 +26,12 @@ namespace WwiseTools.Objects
             this.Type = type;
             //this.Path = WwiseUtility.GetWwiseObjectPath(id);
            // this.Parent = WwiseUtility.GetWwiseObjectByPath(this.Path.Replace(Name, ""));
+        }
+        
+        public static WwiseObject Empty(ObjectType type)
+        {
+            return new WwiseObject("", "{00000000-0000-0000-0000-000000000000}",
+                type.ToString());
         }
 
         public string GetPropertyAndReferenceNames()
