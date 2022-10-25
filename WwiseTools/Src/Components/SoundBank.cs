7cb49fdc0da2ec7ac0172a6cae9f985b935d0945
yangweiqin <yangweiqin@xd.com>
2022-10-25 17:06:44 +0800

    SoundBank组件增加CleanInclusionAsync

--- WwiseTools/Src/Components/SoundBank.cs
+++ WwiseTools/Src/Components/SoundBank.cs
@@ -11,12 +11,17 @@ public class SoundBank : ComponentBase
 {
     public async Task<bool> AddInclutionAsync(SoundBankInclusion inclusion)
     {
-        return await WwiseUtility.Instance.AddSoundBankInclusion(WwiseObject, inclusion);
+        return await WwiseUtility.Instance.AddSoundBankInclusionAsync(WwiseObject, inclusion);
     }
 
     public async Task<List<SoundBankInclusion>> GetInclusionsAsync()
     {
-        return await WwiseUtility.Instance.GetSoundBankInclusion(WwiseObject);
+        return await WwiseUtility.Instance.GetSoundBankInclusionAsync(WwiseObject);
+    }
+
+    public async Task<bool> CleanInclusionAsync()
+    {
+        return await WwiseUtility.Instance.CleanSoundBankInclusionAsync(WwiseObject);
     }
     
     public SoundBank(WwiseObject wwiseObject) : base(wwiseObject, nameof(SoundBank))
